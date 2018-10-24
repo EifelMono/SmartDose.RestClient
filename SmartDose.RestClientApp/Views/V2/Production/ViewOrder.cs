6cd62e37a9cd11c88ae7c7d5cd439629acc5a27d
Andreas Klapperich <Andreas@Klapperich.de>
2018-10-24 14:25:26 +0200

    FIX: Orders V2

--- SmartDose.RestClientApp/Views/V2/Production/ViewOrder.cs
+++ SmartDose.RestClientApp/Views/V2/Production/ViewOrder.cs
@@ -33,6 +33,48 @@ namespace SmartDose.RestClientApp.Views.V2.Production
                 OnButtonExecute = async (self) => self.ResponseObject = await crudInstance.DeleteAsync(
                                         self.RequestParamsValueAsString(0)),
             });
+
+            _tabControl.Items.Add(new ViewTabItem<Models.Production.Order>
+            {
+                Header = "OrderResult (Get)",
+                RequestParams = new List<ViewParam>
+                {
+                    new ViewParam {Name=labelIdName, IsViewObjectJson= false, Value= "" },
+                },
+                OnButtonExecute = async (self) => self.ResponseObject = await crudInstance.GetOrderResult(
+                                        self.RequestParamsValueAsString(0)),
+            });
+
+            _tabControl.Items.Add(new ViewTabItem<Models.Production.Order>
+            {
+                Header = "OrderResultByTime (Get)",
+                RequestParams = new List<ViewParam>
+                {
+                    new ViewParam {Name=labelIdName, IsViewObjectJson= false, Value= "" },
+                },
+                OnButtonExecute = async (self) => self.ResponseObject = await crudInstance.GetOrderResultByTime(
+                                        self.RequestParamsValueAsString(0))
+            });
+
+            _tabControl.Items.Add(new ViewTabItem<Models.Production.Order>
+            {
+                Header = "List of OrderStatus (Get)",
+                RequestParams = new List<ViewParam>
+                {
+                },
+                OnButtonExecute = async (self) => self.ResponseObject = await crudInstance.GetOrderStatus()
+            });
+
+            _tabControl.Items.Add(new ViewTabItem<Models.Production.Order>
+            {
+                Header = "OrderStatus (Get)",
+                RequestParams = new List<ViewParam>
+                {
+                    new ViewParam {Name=labelIdName, IsViewObjectJson= false, Value= "" },
+                },
+                OnButtonExecute = async (self) => self.ResponseObject = await crudInstance.GetOrderStatus(
+                                        self.RequestParamsValueAsString(0))
+            });
         }
     }
 }
