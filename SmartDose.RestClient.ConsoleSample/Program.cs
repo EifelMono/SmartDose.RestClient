606adbca84e16ee5f483b813e5436139983fc0ef
Andreas Klapperich <Andreas@Klapperich.de>
2018-11-08 09:32:52 +0100

    working with Wcf

--- SmartDose.RestClient.ConsoleSample/Program.cs
+++ SmartDose.RestClient.ConsoleSample/Program.cs
@@ -11,11 +11,13 @@ namespace SmartDose.RestClient.ConsoleSample
     {
         static async Task Main(string[] args)
         {
+            await TestAsync();
+
+
             Console.WriteLine($"UrlV1={RestClientGlobals.UrlV1}");
             Console.WriteLine($"UrlV2={RestClientGlobals.UrlV2}");
             Console.WriteLine($"TimeOut={RestClientGlobals.UrlTimeSpan.TotalMilliseconds} ms");
 
-            await TestAsync();
 
             await Rest.V1.MasterData.MasterData.RestTest();
             await Rest.V2.MasterData.MasterData.RestTest();
@@ -26,20 +28,18 @@ namespace SmartDose.RestClient.ConsoleSample
 
         static void Test()
         {
-
-            var _callbackHandler = new MasterDataCallbackHandler();
-            var _masterDataClient =
-                new MasterDataClient(
-                    WellKnownUrl.Get(WellKnownUrlType.SDMasterData,
-                        ConfigurationManager.AppSettings["SmartDoseServer"]), _callbackHandler);
-            _masterDataClient.ConnectionEstablished += _masterDataClient_ConnectionEstablished;
-            _masterDataClient.ConnectionClosed += _masterDataClient_ConnectionClosed;
-            _masterDataClient.Start();
+           
         }
 
         static async Task TestAsync()
         {
-            await Task.Delay(1);
+            WcfClient.Test.FindServiceClientMethods();
+            var o = WcfClient.Test.CreateServiceClient("net.tcp://LWDEU08DTK2PH2:9002/MasterData");
+            var m = (MasterDataServiceClient)o;
+            var c = await m.GetMedicinesAsync(new SearchFilter[] { }, null, null);
+            WcfClient.Test.CreateServiceReference();
+            Console.WriteLine("Wait for Result and return to end");
+            Console.ReadLine();
         }
     }
 }
---
+++ SmartDose.WcfClient/Test.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+
+namespace SmartDose.WcfClient
+{
+    public static class Test
+    {
+        public static void CreateServiceReference()
+        {
+            // var p= Process.Start("dotnet", "svcutil  net.tcp://127.0.0.1:9002/MasterData -n *,SmartDose.WcfClient.MasterData");
+
+            var p = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    WorkingDirectory = @"C:\Dev\müll\HelloSvcutil",
+                    CreateNoWindow = false,
+                    FileName = "cmd.exe",
+                    Arguments = "/C dotnet svcutil  net.tcp://127.0.0.1:9002/MasterData -n *,HellWorld",
+                }
+            };
+            p.Start();
+        }
+
+        public static Type FindServiceClient()
+            => typeof(Test).Assembly.GetTypes()
+                .Where(a => a.GetInterfaces().Where(i => i == typeof(ICommunicationObject)).Any())
+                .Where(t => t.FullName.EndsWith("Client")).FirstOrDefault();
+
+        public static object CreateServiceClient(string endPointAddress)
+            => Activator.CreateInstance(FindServiceClient(),
+                    new NetTcpBinding(SecurityMode.None),
+                    new EndpointAddress(endPointAddress));
+
+        public static void FindServiceClientMethods()
+        {
+            var sb = new StringBuilder();
+            foreach (var m in FindServiceClient().GetMethods())
+            {
+                sb.AppendLine($"{m.IsPublic} {m.IsConstructor} {m.Name}");
+                foreach (var p in m.GetParameters())
+                {
+                    sb.AppendLine($"  {p.Name} {p.ParameterType.Name}");
+                }
+
+            }
+
+            var s = sb.ToString();
+            Debug.WriteLine(sb.ToString());
+        }
+    }
+}
